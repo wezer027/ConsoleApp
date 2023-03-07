4f6019272ec3f9e8b545c13eb8e0795c5c8bf4a0
Wezer <maksim_moskalenk@inbox.ru>
2023-03-07 18:41:58 +0300

    Привет любимый пользователь, только любимый)

---
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "максим")
+{
+    Console.WriteLine("Ура, это же Максим");
+}
+else
+if (username.ToLower() == "ольга")
+{
+    Console.WriteLine("Ура, это же наша любимая Оленька!:");
+}
+else
+if (username.ToLower() == "дима")
+{
+    Console.WriteLine("Привет, бука");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
